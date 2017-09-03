80ca56e733d1809711bab392464cb75d532ef9e3
Naganithin Manne <nithinmanne@gmail.com>
2017-09-03 11:33:29 +0530

    Working until 32 bit

--- mips4/group_1_mips_assgn4_14EE10026_12CS10001.asm
+++ mips4/group_1_mips_assgn4_14EE10026_12CS10001.asm
@@ -126,6 +126,16 @@ malloc:
 
 	jr $ra
 
+unmalloc:
+	#$a0 -> number of addresses to free
+
+	unmallocloop:			#Loop that runs $a0 times
+		addi $sp, $sp, 4  	# Increment stack pointer by 4 -> Deallocating memory of 4 bytes = 32 bits
+		addi $a0, $a0, -1	# Decrement $a0 for loop count
+		bgtz $a0, unmallocloop
+
+	jr $ra
+
 sqMatPrint:
 	#$a0 -> integer n
 	#$a1 -> address of array which is stored in row major
@@ -187,6 +197,8 @@ findDet:
 	sw $t4, 0($sp)		# Save $t2 to stack
 	addi $sp, $sp, -4	# Decrement stack pointer by 4 -> Allocating memory of 4 bytes = 32 bits
 	sw $t5, 0($sp)		# Save $t2 to stack
+	addi $sp, $sp, -4	# Decrement stack pointer by 4 -> Allocating memory of 4 bytes = 32 bits
+	sw $t6, 0($sp)		# Save $t2 to stack
 
 	move $t0, $a0
 	move $t1, $a1
@@ -199,23 +211,23 @@ findDet:
  	move $a0, $t0
  	move $a1, $t1
 
- 	li $t2, 1
+ 	li $t2, 1			#Base case for n=1
  	bne $t0, $t2, findDetNotBase1
- 	lw $v0, 0($t1)
+ 	lw $v0, 0($t1)	#Det = a[0][0]
  	b findDetreturn
  	findDetNotBase1:
- 	li $t2, 2
+ 	li $t2, 2		#Base case check for n=2
  	bne $t0, $t2, findDetNotBase2
  	lw $t0, 0($t1)
- 	lw $t2, 8($t1)
+ 	lw $t2, 12($t1)
  	mul $v0, $t0, $t2
  	lw $t0, 4($t1)
- 	lw $t2, 12($t1)
+ 	lw $t2, 8($t1)
  	mul $t0, $t0, $t2
- 	sub $v0, $v0, $t0
+ 	sub $v0, $v0, $t0	#det= a[0][0]a[1][1]-a[0][1]a[1][0]
  	b findDetreturn
  	findDetNotBase2:
- 	li $t2, 1		#for alternating addition and subtraction
+ 	li $t6, 1		#for alternating addition and subtraction
  	move $t0, $a0	#Loop iterator
  	move $t1, $a1	#Address iterator
  	move $t4, $a0	#Temporary storage for n
@@ -224,17 +236,26 @@ findDet:
  	findDetloop:
  		lw $t3, 0($t1)			#Get current index value
  		move $a0, $t4			#$a0 -> n
+		addi $a0, $a0, -1	#$a0 -> n-1
+		mul $a0, $a0, $a0	#$a0 = (n-1)*(n-1)
+		jal malloc
+		move $a0, $t4			#$a0 -> n
  		move $a1, $t0			#$a1 -> k row iterating upon
  		move $a2, $t5			#$a2 -> address of old array
+		move $a3, $v0			#$a3 -> new address
  		jal makeArr
  		move $a0, $v0			#$v0 -> new n = n-1
  		move $a1, $v1			#$v1 -> new array address
  		jal findDet 			#Recursive Call
- 		mul $t3, $t3, $v1		#$t3 = value * sub-determinant
- 		mul $t3, $t3, $t2		#plus or minus
+ 		mul $t3, $t3, $v0		#$t3 = value * sub-determinant
+ 		mul $t3, $t3, $t6		#plus or minus
  		add $t2, $t2, $t3		#add to total
  		li $t3, -1				#invert sign for next time
- 		mul $t2, $t2, $t1
+ 		mul $t6, $t6, $t3
+		move $a0, $t4			#$a0 -> n
+		addi $a0, $a0, -1	#$a0 -> n-1
+		mul $a0, $a0, $a0	#$a0 = (n-1)*(n-1)
+		jal unmalloc
  		addi $t1, $t1, 4
  		addi $t0, $t0, -1
  		bgtz $t0, findDetloop
@@ -253,6 +274,8 @@ findDet:
  	syscall				#syscall for printing 'newl'
  	move $v0, $t2
 
+	lw $t6, 0($sp)		# Pop $t2 from stack
+	addi $sp, $sp, 4	# Increment stack pointer by 4 -> De-allocating memory of 4 bytes = 32 bits
 	lw $t5, 0($sp)		# Pop $t2 from stack
 	addi $sp, $sp, 4	# Increment stack pointer by 4 -> De-allocating memory of 4 bytes = 32 bits
 	lw $t4, 0($sp)		# Pop $t2 from stack
@@ -276,6 +299,7 @@ makeArr:
 	#$a0 -> integer n
 	#$a1 -> index k to avoid to put
 	#$a2 -> address of array which is stored in row major
+	#$a3 -> address new array
 	#$v0 -> returns new n = n-1
 	#$v1 -> returns address
 
@@ -298,30 +322,29 @@ makeArr:
 
 	move $t3, $a0		#Store n
 	addi $t0, $a0, -1	#$t0 = n-1
-	move $t1, $sp 		#Store $sp
-	mul $a0, $t0, $t0	#$a0 = (n-1)*(n-1)
-	jal malloc 			#malloc for new array
-	move $sp, $t1		#Recover $sp
-	move $t1, $v0		#$t1 = address of new array
-	move $v1, $v0		#$v1 = address of new array
+	move $t1, $a3		#$t1 = address of new array
+	move $v1, $a3		#$v1 = address of new array
 	move $v0, $t0		#$v0 = n-1
 	move $t0, $t3		#$t0 = n
 	move $a0, $t3
 	add $t0, $t0, $t0
 	add $t0, $t0, $t0		#Quadrupling address
 	add $t0, $t0, $a2		#Address iterator
-	move $t1, $0			#Index iterator = n-1
+	move $t1, $v0			#Index iterator = n-1
 	move $t2, $v1			#New array address iterator
 	makeArrouter:
-		move $t3, $v0
+		move $t3, $v0			#Inner loop counter
+		addi $t3, $t3, 1	#Inner loop iterator = n
 		makeArrinner:
-			lw $t4, 0($t0)
-			addi $t0, $t0, 4
-			beq $a1, $t3, makeArrinner
+			lw $t4, 0($t0)		#Get word from old array
+			addi $t0, $t0, 4	#Increment old array iterator
+			beq $a1, $t3, makeArrinnerskip	#Check if skippable row
 			sw $t4, 0($t2)
 			addi $t2, $t2, 4
+			makeArrinnerskip:
 			addi $t3, $t3, -1
 			bgtz $t3, makeArrinner
+		addi $t1, $t1, -1
 		bgtz $t1, makeArrouter
 
 
